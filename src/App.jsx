5130d5b1ebc4923467984f198d2e3082ec7ace78
kahkasha1711 <kahkashakhan1417@gmail.com>
2024-10-25 21:45:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from "flowbite-react";
+import { Alert } from "flowbite-react";
+
+
+
+function App() {
+  
+  return (
+    <>
+     <h1>App.JSX</h1>
+    </>
+  )
+}
+
+export default App
