a22176c6f35504f8bc91d533188a6bcdf4a1bae3
Kahkasha17 <kahkashakhan1417@gmail.com>
2024-11-02 15:54:04 +0530

    home page completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
     <>
       <section className="bg-gray-100 dark:bg-gray-900 ">
         <div className="mt-10 mx-auto lg:py-16 h-full ">
-          <div className="rounded-lg p-10 bg-[url('./homebk.jpg')] bg-contain">
+          <div className="rounded-lg p-10 bg-[url('./homebg.avif')] bg-cover bg-no-repeat">
             <div className="grid md:grid-cols-2 gap-4 justify-center">
               <Banner />
               <LoginForm />
             </div>
           </div>
-          <div className="w-full "><HorizontalLine/></div>
+          {/* <div className="w-full "><HorizontalLine/></div> */}
           
         </div>
       </section>
