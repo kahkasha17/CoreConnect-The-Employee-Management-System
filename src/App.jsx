09709d036577b6a27711a83305603141d5a19cc4
Kahkasha17 <kahkashakhan1417@gmail.com>
2024-11-06 20:35:31 +0530

    directory changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import './App.css'
 import React from 'react'
 import Main from './Pages/Main'
-import { EmployeeDashboard } from './Pages/Dashboard/Employee/EmployeeDashboard'
-import Footers from './Components/Footers'
+import { EmployeeDashboard } from './Pages/Dashboards/Employee/EmployeeDashboard'
 
 const App = () => {
   return (
