385ef5488147bea1feaea918581546e1d1219426
Kahkasha17 <kahkashakhan1417@gmail.com>
2024-11-09 17:58:59 +0530

    homebackground added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <>
       <section className="bg-gray-100 dark:bg-gray-900 ">
         <div className="mt-10 mx-auto lg:py-16 h-full ">
-          <div className="rounded-lg p-10 bg-[url('./homebg.avif')] bg-cover bg-no-repeat">
+          <div className="rounded-lg p-10 bg-[url('./Homeback.jpg')] bg-cover bg-no-repeat">
             <div className="grid md:grid-cols-2 gap-4 justify-center">
               <Banner />
               <LoginForm />
