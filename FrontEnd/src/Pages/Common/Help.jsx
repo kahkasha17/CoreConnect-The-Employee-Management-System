e47f9683809354c527d3e23cec0c9c8cf6c8bf51
Kahkasha17 <kahkashakhan1417@gmail.com>
2024-11-26 21:58:52 +0530

    help completed

--- FrontEnd/src/Pages/Common/Help.jsx
+++ FrontEnd/src/Pages/Common/Help.jsx
@@ -1,8 +1,14 @@
+"use client";
+
 import React from 'react'
+import { Alert } from "flowbite-react";
+
 
 export const Help = () => {
   return (
-    <div>Help</div>
+    <div className='min-h-screen'><Alert color="teal" className='font-bold' rounded>
+     For any issues or concerns, kindly reach out to us via email at <span className='font-extrabold text-red-600'>codewithkahkasha1517@gmail.com</span> or contact the IT Manager within your department for further assistance.
+  </Alert></div>
   )
 }
 
